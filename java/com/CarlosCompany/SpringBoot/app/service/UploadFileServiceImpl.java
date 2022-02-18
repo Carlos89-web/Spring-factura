059b7583195b6d022872fe632827f7805a074ddb
Carlos89-web <79230085+Carlos89-web@users.noreply.github.com>
2022-02-18 11:01:56 +0100

    Update UploadFileServiceImpl.java

--- java/com/CarlosCompany/SpringBoot/app/service/UploadFileServiceImpl.java
+++ java/com/CarlosCompany/SpringBoot/app/service/UploadFileServiceImpl.java
@@ -16,8 +16,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-// Video 153
-
 @Service
 public class UploadFileServiceImpl implements IUploadFileService {
 
